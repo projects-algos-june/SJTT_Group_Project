57b6cf0453b64e4c0b5551aa74625be320edcc92
boeschthomas <boeschthomasm@gmail.com>
2020-07-08 11:17:00 -0500

    models.py changes

--- store/models.py
+++ store/models.py
@@ -6,14 +6,17 @@ class UserManager(models.Manager):
     def validator(self, postData):
         errors = {}
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
         if len(postData['fn_input']) < 2:
             errors ['first_name'] = "Please add First Name that contains more the two letters"
         if len(postData['ln_input']) < 2:
             errors ['last_name'] = "Please add a Last Name that contains more the two letters"
         if not EMAIL_REGEX.match(postData['email_input']):
             errors['email'] = 'Please enter a valid Email address!'
-        if len(postData['password_input']) < 5:
-             errors['password'] = 'Please enter an email that contains 5 or more character'
+        # elif User.objects.filter(email=postData['email']).exists():
+        #     errors['email'] = "Email already registered, please login."
+        if len(postData['password_input']) < 5:            
+            errors['password'] = 'Please enter an email that contains 5 or more character'
         if postData['confirmpw_input'] != postData['password_input']:
             errors['confirm_pw'] = "Your password and what you typed in comfirm pw dont match try agian"
         return errors
@@ -23,10 +26,6 @@ class UserManager(models.Manager):
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(postData['email_input']):
             errors['email'] = 'Please enter a valid Email address!'
-        elif not postData['email_input'] != Customer.objects.filter('email'):
-            errors['email'] = 'Email not found !'
-
-
         if len(postData['password_input']) < 5:
              errors['password'] = 'Please enter an email that contains 5 or more character'
         if postData['confirmpw_input'] != postData['password_input']:
@@ -35,10 +34,13 @@ class UserManager(models.Manager):
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
+    password = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
 
     def __str__(self):
         return self.name
